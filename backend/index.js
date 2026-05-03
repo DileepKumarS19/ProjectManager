db5dafb221804d9d2a0302206fe978e46684e94e
dileepdili <dileepkumardilideepu@gmail.com>
2026-05-03 15:25:24 +0530

    env variables modifications

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ import dashboardRouter from "./routes/dashboard.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 const startServer = async () => {
